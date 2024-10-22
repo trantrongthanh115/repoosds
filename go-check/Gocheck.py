57a3dc223a074f008b92f0ee75395d666476941d
trantrongthanh115 <trongthanh1152004@gmail.com>
2024-10-22 08:09:03 +0700

    capnhat

--- go-check/Gocheck.py
+++ go-check/Gocheck.py
@@ -10,7 +10,7 @@ import time
 import pandas as pd
 
 # Đường dẫn đến file thực thi geckodriver
-gecko_path = r"D:/Gocheck/geckodriver.exe"
+gecko_path = r"C:/Users/Administrator/Downloads/geckodriver.exe"
 
 # Khởi tởi đối tượng dịch vụ với đường geckodriver
 ser = Service(gecko_path)
